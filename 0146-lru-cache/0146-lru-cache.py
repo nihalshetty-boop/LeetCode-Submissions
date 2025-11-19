87d14fe09a4e55f7a3de5da5748b3b0cbf22c7cd
Nihal Shetty <nihalshetty2001@gmail.com>
2025-11-18 22:40:07 -0500

    Time: 170 ms (22.02%), Space: 77.9 MB (71.73%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -11,22 +11,12 @@ class LRUCache:
         self.left = self.right = Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
-    def remove(self, node):
-        prev, nxt =  node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-        
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        else:
-            return -1
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -39,6 +29,14 @@ class LRUCache:
             self.remove(lru)
             del self.cache[lru.key]
 
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
